2bda640cdee8d480b298c43d2dd34c0371c663bb
Baron Alexandre <la188784@student.helha.be>
2021-03-27 16:23:33 +0100

    Ressource_controller

--- LesAffreux2021/Assets/Scripts/Ressource.cs
+++ LesAffreux2021/Assets/Scripts/Ressource.cs
@@ -9,34 +9,40 @@ public class Ressource : MonoBehaviour
     public int current_max_capacity; //  Current capacity for this.ressource => public because we can put in unity the value for this ressource
     private int _current_ressource = 0; // Current value of ressource
     //private float _multiplicator = 1; // Multiplicator value of ressource
-    public string ressource_name;
+    public Resource_type ressource_name;
     public Slider slider;
 
-    public void increase_value(int value) { // Increase the current value of the ressource
+    public void increase_value(int value) 
+    { // Increase the current value of the ressource
         this._current_ressource += value;
-        if (this.current_max_capacity <= this._current_ressource) {
+        if (this.current_max_capacity <= this._current_ressource) 
+        {
             this._current_ressource = this.current_max_capacity;
         }
         this.slider.value = this._current_ressource;
         text.text = ressource_name  + " " + _current_ressource.ToString() + "/" + current_max_capacity.ToString();
     }
 
-    public void decrease_value(int value) { // Decrease the current value of the ressource
+    public void decrease_value(int value) 
+    { // Decrease the current value of the ressource
         this._current_ressource -= value;
-        if (this._current_ressource <= 0) {
+        if (this._current_ressource <= 0) 
+        {
             this._current_ressource = 0;
         }
         this.slider.value = this._current_ressource;
         text.text = ressource_name  + " " + _current_ressource.ToString() + "/" + current_max_capacity.ToString();
     }
 
-    public void increase_capacity(int value) { // Increase the current max capacity of the ressource
+    public void increase_capacity(int value) 
+    { // Increase the current max capacity of the ressource
         this.current_max_capacity += value;
         this.slider.maxValue = this.current_max_capacity;
         text.text = ressource_name  + " " + _current_ressource.ToString() + "/" + current_max_capacity.ToString();
     }
 
-    public void decrease_capacity(int value) { // Decrease the current max capacity of the ressource
+    public void decrease_capacity(int value) 
+    { // Decrease the current max capacity of the ressource
         this.current_max_capacity -= value;
         this.slider.maxValue = this.current_max_capacity;
         text.text = ressource_name  + " " + _current_ressource.ToString() + "/" + current_max_capacity.ToString();
--- LesAffreux2021/Assets/Scripts/Ressource_Controller.cs
+++ LesAffreux2021/Assets/Scripts/Ressource_Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Population : MonoBehaviour
+public class Ressource_Controller : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
